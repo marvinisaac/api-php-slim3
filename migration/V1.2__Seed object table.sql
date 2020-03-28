b1f74e0ae4531f8a0360d588e7881165abfae57b
marvin <marvin@marvinisaac.com>
2020-03-28 11:43:02 +0800

    :card_file_box: Seed object table

---
+++ migration/V1.2__Seed object table.sql
@@ -0,0 +1,10 @@
+INSERT INTO `object` (`name`) VALUES ('first');
+INSERT INTO `object` (`name`) VALUES ('second');
+INSERT INTO `object` (`name`) VALUES ('third');
+INSERT INTO `object` (`name`) VALUES ('fourth');
+INSERT INTO `object` (`name`) VALUES ('fifth');
+INSERT INTO `object` (`name`) VALUES ('sixth');
+INSERT INTO `object` (`name`) VALUES ('seventh');
+INSERT INTO `object` (`name`) VALUES ('eighth');
+INSERT INTO `object` (`name`) VALUES ('ninth');
+INSERT INTO `object` (`name`) VALUES ('tenth');
