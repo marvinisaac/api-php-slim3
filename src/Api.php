984a0e9d8b26baf1fc8ac86d27f9eaca81f4b266
Marvin Isaac <marvin@marvinisaac.com>
2020-12-13 23:52:33 +0000

    :white_check_mark: Create API class

--- src/Api.php
+++ src/Api.php
@@ -1,9 +1,23 @@
 <?php
     namespace Api;
 
+    use \Slim\App;
+    use \Slim\Http\Request;
+    use \Slim\Http\Response;
+
 final class Api
 {
+    private $api;
+
+    public function __construct()
+    {
+        $api = new App();
+
+        $this->api = $api;
+    }
+
     public function get()
     {
+        return $this->api;
     }
 }
