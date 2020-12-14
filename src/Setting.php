709e0a88b0f91c45bebef9de07adedecc550ff34
Marvin Isaac <marvin@marvinisaac.com>
2020-12-14 02:52:59 +0000

    :white_check_mark: Add settings before creating API

--- src/Setting.php
+++ src/Setting.php
@@ -5,8 +5,16 @@ final class Setting
 {
     public static function get() : array
     {
+        $environment = $_ENV['PHP_ENVIRONMENT'] ?? null;
+        $isDisplayErrorDetails = false;
+        if ($environment === 'LOCAL' || $environment === 'TEST') {
+            $isDisplayErrorDetails = true;
+        }
+
         return [
-            'settings' => [],
+            'settings' => [
+                'displayErrorDetails' => $isDisplayErrorDetails,
+            ],
         ];
     }
 }
