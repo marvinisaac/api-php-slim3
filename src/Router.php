dd4102a729e2ce19c10e906e58bc1f77d9a211e1
Marvin Isaac <marvin@marvinisaac.com>
2020-12-14 03:43:02 +0000

    :white_check_mark: Add basic routes

--- src/Router.php
+++ src/Router.php
@@ -9,6 +9,18 @@ final class Router
 {
     public static function add(App $api) : App
     {
+        $api->get('/health', function (Request $request, Response $response, array $args) {
+            $response = $response->withStatus(200);
+            $response = $response->withJson([
+                'time' => date('Y-m-d H:i:s'),
+            ]);
+            return $response;
+        });
+
+        $api->any('/[{path:.*}]', function (Request $request, Response $response, array $args) {
+            return $response->withStatus(404);
+        });
+
         return $api;
     }
 }
